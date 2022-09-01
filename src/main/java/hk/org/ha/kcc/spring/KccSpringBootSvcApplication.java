541aeb546a406da0510774ce63e59ddeaf354800
Kevin Tsang <thh703@ha.org.hk>
2022-09-01 11:14:50 +0800

    Initialize project using Spring Initializr

---
+++ src/main/java/hk/org/ha/kcc/spring/KccSpringBootSvcApplication.java
@@ -0,0 +1,13 @@
+package hk.org.ha.kcc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KccSpringBootSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KccSpringBootSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hk/org/ha/kcc/spring/KccSpringBootSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package hk.org.ha.kcc.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KccSpringBootSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
