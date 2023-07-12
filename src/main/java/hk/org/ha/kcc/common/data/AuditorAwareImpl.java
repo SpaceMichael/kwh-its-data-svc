e04cadf7541c5ba72008ed036859ee7af91e74d9
lh315 <lh315@ha.org.hk>
2023-07-12 16:58:29 +0800

    update auditorAwareImpl

--- src/main/java/hk/org/ha/kcc/common/data/AuditorAwareImpl.java
+++ src/main/java/hk/org/ha/kcc/common/data/AuditorAwareImpl.java
@@ -4,19 +4,16 @@ import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
 
   @Override
   public Optional<String> getCurrentAuditor() {
     return Optional.ofNullable(SecurityContextHolder.getContext())
-        .map(SecurityContext::getAuthentication)
+        .map(context -> (JwtAuthenticationToken) context.getAuthentication())
         .filter(Authentication::isAuthenticated)
-        .map(Authentication::getPrincipal)
-        .map(UserDetails.class::cast)
-        .map(UserDetails::getUsername);
+        .map(authentication -> authentication.getTokenAttributes().get("preferred_username").toString());
   }
 }
