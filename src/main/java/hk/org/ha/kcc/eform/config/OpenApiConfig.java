2c38e9f51942a84999b60509e267b0397b0aeb05
Kevin Tsang <thh703@ha.org.hk>
2023-10-18 17:37:23 +0800

    Add liveness and readiness probe to OpenShift deployment.

--- src/main/java/hk/org/ha/kcc/eform/config/OpenApiConfig.java
+++ src/main/java/hk/org/ha/kcc/eform/config/OpenApiConfig.java
@@ -6,10 +6,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(info = @Info(title = "KCC Spring Boot Service", version = "1.0"),
-        servers = {@Server(url = "/", description = "Default Server URL")})
+@OpenAPIDefinition(info = @Info(title = "KWH ITS e-Form Service", version = "1.0"),
+    servers = {@Server(url = "/", description = "Default Server URL")})
 @SecurityScheme(name = "JWT", type = SecuritySchemeType.HTTP, scheme = "bearer",
-        bearerFormat = "JWT")
+    bearerFormat = "JWT")
 public class OpenApiConfig {
 
 }
