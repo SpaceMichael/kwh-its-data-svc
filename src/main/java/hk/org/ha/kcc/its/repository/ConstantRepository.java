2ce04d35c77366def647fcf1122a52592b862475
MichaelTse <ttk799@kwh.ha.org.hk>
2023-11-06 18:39:45 +0800

    remove constant

--- src/main/java/hk/org/ha/kcc/its/repository/ConstantRepository.java
+++
@@ -1,8 +0,0 @@
-package hk.org.ha.kcc.its.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import hk.org.ha.kcc.its.model.Constant;
-
-public interface ConstantRepository extends JpaRepository<Constant, Integer> {
-
-}
--- src/main/java/hk/org/ha/kcc/its/service/GetUsrIdService.java
+++
@@ -1,6 +0,0 @@
-package hk.org.ha.kcc.its.service;
-
-public interface GetUsrIdService {
-
-  String getUserId();
-}
