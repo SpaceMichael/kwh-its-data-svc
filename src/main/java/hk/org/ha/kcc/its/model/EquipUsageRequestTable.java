2df2b3bc1bab5ac00e643edb774c26e9f9e60fe5
MichaelTse <ttk799@kwh.ha.org.hk>
2023-12-01 11:13:03 +0800

    add cleansning svg

---
+++ src/main/java/hk/org/ha/kcc/its/model/EquipUsageRequestTable.java
@@ -0,0 +1,40 @@
+package hk.org.ha.kcc.its.model;
+
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.*;
+
+/*@Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity*/
+//@Audited
+//@Table(name = "equip_usage_request")
+public class EquipUsageRequestTable {
+
+    /*@Id
+    @Column(length = 12)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private String id;*/
+    /*
+    private String hospitalCode; // e.g KWH
+    private String dept; // e.g M&G
+    private String ward; // e.g 12BMN
+    private String cubicle; // e.g 1
+    private Integer eamNo; // 1824496
+    SerNo;// E09-123567890123
+    Model; //ELITDESK PU 8000G5 SFF
+    BelongTo;// KWH Ward 8A
+    Type; //MED
+    CaseNo; //e.g HN123454677
+    PatientName; // KC
+    Date; //e.g 2023-12-31
+    Time; //from frontend e.g 10:00
+    User; //get from token
+
+     */
+}
