c3ee7fe0c53d9a858e7d01199e1ee5beb1616b86
MichaelTse <ttk799@kwh.ha.org.hk>
2024-01-16 14:29:01 +0800

    test kwh ts key 2

--- src/main/java/hk/org/ha/kcc/common/security/ApiKeyUtil.java
+++ src/main/java/hk/org/ha/kcc/common/security/ApiKeyUtil.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ApiKeyUtil {
-    @Value("${api.key}")
+    @Value("5f2f3d82-94cc-4a0a-b731-b61d8a2631d2")
     private String apiKey;
 
     public boolean validateApiKey(String authApiKey) {
